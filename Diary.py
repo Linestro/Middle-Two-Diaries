d38c755366e6651da2d71f8b7d02dcadd5e10a10
Haojie Ye <yehaojie@umich.edu>
2019-10-28 15:16:03 -0400

    penny dreadful

--- Diary.py
+++ Diary.py
@@ -1,6 +1,6 @@
 Tiffany's Breakfast: 
 Paul: Holly I am in love with you.
-Holly: So what.
+Holly (Audrey): So what.
 Paul: So what? So plenty! I love you, you belong to me!
 Holly: [tearfully] No. People don't belong to people.
 Paul: Of course they do!
@@ -13,3 +13,14 @@ Holly: I'm not Holly. I'm not Lulamae either. I don't know who I am. I'm like ca
 Paul: You call yourself a free spirit, a "wild thing," and you're terrified somebody's gonna stick you in a cage. Well baby, you're already in that cage. You built it yourself. And it's not bounded in the west by Tulip, Texas, or in the east by Somali-land. It's wherever you go. Because no matter where you run, you just end up running into yourself.
 Paul: I've been carrying this thing around for months. I don't want it anymore. 
 
+Penny Dreadful:
+Vanessa (Eva Green): Are you leaving? 
+Monster (Frankelin's creature): Yes. 
+Vanessa: For long? 
+Monster: For ever. 
+Vanessa: Where will you go?
+Monster: Where I belong. Away from mankind. 
+Vanassa: I think you are the most human man... I have ever known. 
+Monster: This dream I had ... This long dream of kinship with those unlike me, it is gone. When you’ve seen that of which you are capable, when you’ve stood in blood long enough ... What is left but to wade into a delosate shore? Away from all others. 
+
+Cowboy Bebop: 
