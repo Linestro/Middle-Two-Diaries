0afc711d747fef5f52abd2c8db5f9a80f3c2e7c3
Haojie Ye <yehaojie@umich.edu>
2019-11-14 18:52:32 -0500

    lazy commit

--- mtx2coo.py
+++ mtx2coo.py
@@ -1,3 +1,13 @@
 from scipy.io import mmread
+import numpy as np
+from scipy import sparse
 A = mmread('bwm200.mtx')
-print(A.toarray())
+# Coo = sparse.coo_matrix(A.toarray())
+B = A.toarray()
+# print(B.shape)
+cnt = 0
+for i in range(B.shape[0]):
+    for j in range(B.shape[1]):
+        if B[i][j] != 0:
+            print('cooRowIndexHostPtr[' + str(cnt) +']=' + str(i) + '; cooColIndexHostPtr[' + str(cnt) + ']=' + str(j) + '; cooValHostPtr[' + str(cnt) + ']=' + str(B[i][j]) + ';')
+            cnt += 1
